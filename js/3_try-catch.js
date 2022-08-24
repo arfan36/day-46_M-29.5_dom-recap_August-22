4091dbb0ea5f20c8a189cc2dfcd1c9f26470e351
arfan36 <arfanur36@gmail.com>
2022-08-24 18:46:19 +0600

    29_5-4 (semi-advanced) A simple introduction to Date

--- js/3_try-catch.js
+++ js/3_try-catch.js
@@ -1,24 +1,24 @@
 const day = 'day the din';
-console.log(day);
-console.log(25);
+// console.log(day);
+// console.log(25);
 // console.log(district);
 // let district = 'bBaria';
-console.log(100);
-console.log(100);
-console.log(100);
+// console.log(100);
+// console.log(100);
+// console.log(100);
 
 try {
-    console.log('inside try');
+    // console.log('inside try');
     // console.log(student);
-    console.log(district);
+    // console.log(district);
     let district = 'bBaria';
-    console.log('after error');
+    // console.log('after error');
 }
 catch (error) {
-    console.log('inside catch');
+    // console.log('inside catch');
     // console.log(error);
 }
 finally {
-    console.log('finally done');
+    // console.log('finally done');
 }
-console.log('last line of the code');
+// console.log('last line of the code');
