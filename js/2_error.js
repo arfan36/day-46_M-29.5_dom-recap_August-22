28427403b5c2c5a2e8caaaeb82b6218801a0c0ea
arfan36 <arfanur36@gmail.com>
2022-08-24 20:00:16 +0600

    add comment

--- js/2_error.js
+++ js/2_error.js
@@ -1,14 +1,14 @@
 // not in JS that frequently: range error
-const numbers = [3, 4, 5, 8];
+// const numbers = [3, 4, 5, 8];
 // you should not do it (most of the time)
-numbers.length = 2;
+// numbers.length = 2;
 // console.log(numbers);
 // console.log(numbers.length);
 // console.log(numbers[25]);
 
 // reference Error
 // console.log(money);
-const money = 80;
+// const money = 80;
 
 // syntax Error
 // for(let i=0; i<5 i++){
@@ -18,5 +18,5 @@ const money = 80;
 // }
 // else
 
-let student = { address: 'kochukhet' };
+// let student = { address: 'kochukhet' };
 // console.log(student.address.city);
